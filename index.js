dee29e86c1e1c481caf81081b18849855d8c9355
unknown <iblaudi@gmail.com>
2019-01-13 20:42:24 +0700

    fix(remove cluster for now): herocu cant deploy with cluster

--- index.js
+++ index.js
@@ -1,28 +1,5 @@
-
+const app = require('./app.js');
 const port = process.env.PORT || 3000;
 
-const cluster = require('cluster');
-const http = require('http');
-const numCPUs = require('os').cpus().length;
 
-if (cluster.isMaster) {
-    console.log(`Master ${process.pid} is running`);
-  
-    // Fork workers.
-    for (let i = 0; i < numCPUs; i++) {
-      cluster.fork();
-    }
-  
-    cluster.on('exit', (worker, code, signal) => {
-      console.log(`worker ${worker.process.pid} died`);
-    });
-  } else {
-    // Workers can share any TCP connection
-    // In this case it is an HTTP server
-    const app = require('./app.js');
-    app.listen(port, () =>console.log(`Server started on ${port}`));
-  
-    console.log(`Worker ${process.pid} started`);
-  }
-
-
+app.listen(port, () =>console.log(`Server started on ${port}`));
