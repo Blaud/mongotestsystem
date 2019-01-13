b6d7ecb6c7775a1ad98138f00c13991b1349e5a1
unknown <iblaudi@gmail.com>
2019-01-13 20:33:20 +0700

    fix(disable cluster): heroku dont work with cluster

--- app.js
+++ app.js
@@ -1,33 +1,5 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const passport = require("passport");
-const path = require("path");
-const fs = require("fs");
+const app = require('./app.js');
+const port = process.env.PORT || 3000;
 
-const keys = require("./config/keys");
-const messageRoutes = require("./routes/message");
 
-const app = express();
-
-mongoose
-  .connect(keys.mongoURI, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false })
-  .then(() => console.log("mongoDB connected"))
-  .catch(error => console.log(error));
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-app.use("/api/message", messageRoutes);
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/dist/client"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, "client", "dist", "client", "index.html")
-    );
-  });
-}
-
-module.exports = app;
+app.listen(port, () =>console.log(`Server started on ${port}`));
