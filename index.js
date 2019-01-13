2155de617d2b9237efa0bbccec78d8c9e1ae4859
unknown <iblaudi@gmail.com>
2019-01-13 20:45:47 +0700

    fix(fix briken app.js): copy pasted index to app:(

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const app = require('./app.js');
 const port = process.env.PORT || 3000;
 
 
-app.listen(port, () =>console.log(`Server started on ${port}`));
+app.listen(port, () =>console.log(`Server started on ${port}`)); 
